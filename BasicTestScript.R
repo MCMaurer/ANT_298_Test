e80aee7cf10cb8e3a8abee7837ff8478cdb685ae
Michael Culshaw-Maurer <mjculshawmaurer@ucdavis.edu>
2016-11-01 13:58:50 -0700

    linear regression
    did a simple linear regression

--- BasicTestScript.R
+++ BasicTestScript.R
@@ -3,3 +3,16 @@
 x <- rnorm(n=1000, mean=0, sd=3)
 
 hist(x, nclass=20)
+
+
+## doing a linear regression, m and b are randomly chosen. epsilon is noise (residuals) around 0
+m <- 3
+b <- -2
+epsilon <- rnorm(1000, mean=0, sd=30)
+
+x <- runif(1000, 0, 100)
+
+y <- m*x+b+epsilon
+
+plot(x,y, cex=0.5)
+
